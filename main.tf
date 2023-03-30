33febc6e4f13def338b1a660e866db91fc3ba6fa
JaislinBajwa <89400877+JaislinBajwa@users.noreply.github.com>
2023-03-30 13:27:15 +0530

    Add files via upload

--- main.tf
+++ main.tf
@@ -2,5 +2,16 @@
 
 resource "local_file" "file_block" {
   filename="hello.py"
-  content="<p>Hello world<p>"
+  content="${random_id.random_value.dec}"
+}
+resource "local_file1" "file_block1"{
+  filename="hello1.py"
+  content=local.con
+}
+locals{
+  con="<p> Hello world <p>"
+}
+resource "random_id" "random_value"{
+    byte_length=8
+
 }
